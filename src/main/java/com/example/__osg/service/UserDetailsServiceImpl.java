f4b1312eaaeee158d470e204ac4c8de763a64b73
hadrihl <hadrihl@outlook.com>
2024-11-13 14:41:27 +0800

    watermark accessed by {user}.

--- src/main/java/com/example/__osg/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/__osg/service/UserDetailsServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.__osg.service;
 
+import java.util.Date;
+
+import org.hibernate.type.descriptor.jdbc.TimestampUtcAsOffsetDateTimeJdbcType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.example.__osg.controller.LoggingController;
 import com.example.__osg.entity.User;
 import com.example.__osg.repository.UserRepository;
 
@@ -12,6 +18,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -20,6 +28,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		if(user == null) {
 			throw new UsernameNotFoundException("User not found.");
 		}
+		
+		logger.info("Accessed by user '{}' ", user.getUsername());
 
 		return new CustomUserDetails(user);
 	}
